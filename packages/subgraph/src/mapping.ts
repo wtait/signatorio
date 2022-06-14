7f124dfee8060ecbc8775ac928d4ffcc0dc13bbc
wtait <wade@cgnetworks.dev>
2022-06-14 10:58:58 -0700

    dynamically read walletfactory address for subgraph.yaml file

--- packages/subgraph/src/mapping.ts
+++ packages/subgraph/src/mapping.ts
@@ -1,34 +1,75 @@
 import { BigInt, Address } from "@graphprotocol/graph-ts"
 import {
-  YourContract,
-  SetPurpose
-} from "../generated/YourContract/YourContract"
-import { Purpose, Sender } from "../generated/schema"
+  WalletCreated
+} from "../generated/WalletFactory/WalletFactory"
 
-export function handleSetPurpose(event: SetPurpose): void {
+// import {
+//   WalletFactory
+// } from "../generated/WalletFactory/WalletFactory"
 
-  let senderString = event.params.sender.toHexString()
+import { Wallet, Owner } from "../generated/schema"
 
-  let sender = Sender.load(senderString)
 
-  if (sender == null) {
-    sender = new Sender(senderString)
-    sender.address = event.params.sender
-    sender.createdAt = event.block.timestamp
-    sender.purposeCount = BigInt.fromI32(1)
+export function handleWalletCreated(event: WalletCreated): void {
+//   event WalletCreated (
+//     address indexed _Wallet,
+//     address indexed _Owner, 
+//     address indexed _Caller
+// );
+
+  // type Wallet @entity {
+  //   id: ID!
+  //   owner: Owner!
+  //   address: Bytes!
+  //   createdAt: BigInt!
+  //   transactionHash: String!
+
+  // type Owner @entity {
+  //   id: ID!
+  //   wallets: [Wallet!] @derivedFrom(field: "owner")
+
+  let ownerAddress = event.params._Owner.toHexString()
+  let owner = Owner.load(ownerAddress)
+
+  if(owner == null) {
+    owner = new Owner(ownerAddress)
   }
-  else {
-    sender.purposeCount = sender.purposeCount.plus(BigInt.fromI32(1))
-  }
 
-  let purpose = new Purpose(event.transaction.hash.toHex() + "-" + event.logIndex.toString())
+  let wallet = new Wallet(event.params._Wallet.toHex() + "-" + event.transaction.hash.toHex())
+  wallet.owner = event.params._Owner.toHex()
+  wallet.address = event.params._Wallet
+  wallet.createdAt = event.block.timestamp
+  wallet.transactionHash = event.transaction.hash.toHex()
 
-  purpose.purpose = event.params.purpose
-  purpose.sender = senderString
-  purpose.createdAt = event.block.timestamp
-  purpose.transactionHash = event.transaction.hash.toHex()
-
-  purpose.save()
-  sender.save()
+  owner.save()
+  wallet.save()
 
 }
+
+// export function handleSetPurpose(event: SetPurpose): void {
+
+//   let senderString = event.params.sender.toHexString()
+
+//   let sender = Sender.load(senderString)
+
+//   if (sender == null) {
+//     sender = new Sender(senderString)
+//     sender.address = event.params.sender
+//     sender.createdAt = event.block.timestamp
+//     sender.purposeCount = BigInt.fromI32(1)
+//   }
+//   else {
+//     sender.purposeCount = sender.purposeCount.plus(BigInt.fromI32(1))
+//   }
+
+//   let purpose = new Purpose(event.transaction.hash.toHex() + "-" + event.logIndex.toString())
+
+//   purpose.purpose = event.params.purpose
+//   purpose.sender = senderString
+//   purpose.createdAt = event.block.timestamp
+//   purpose.transactionHash = event.transaction.hash.toHex()
+
+//   purpose.save()
+//   sender.save()
+
+// }
